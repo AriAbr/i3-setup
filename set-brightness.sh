939848608fc0830eafd040db8d52587f578ddb73
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-08-30 11:42:37 +0300

    fix: Set brightness on start

---
+++ set-brightness.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for ((i=1; i<=20; i++))
+do
+    gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Screen.StepUp
+done
